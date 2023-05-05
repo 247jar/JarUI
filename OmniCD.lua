9bf5b1be3a6f532d0927c4a5d929d8fedd4a2b36
247jar <132698335+247jar@users.noreply.github.com>
2023-05-05 23:07:26 +0200

    uploaded JarUI account-wide OmniCD profile

---
+++ OmniCD.lua
@@ -0,0 +1,107 @@
+
+OmniCDDB = {
+	["profileKeys"] = {
+		["Jarpd - Tarren Mill"] = "JarUI",
+		["Mageblob - Archimonde"] = "Default",
+		["Jarmk - Tarren Mill"] = "Default",
+		["Jarw - Tarren Mill"] = "JarUI",
+		["Jardr - Tarren Mill"] = "Default",
+		["Jarpd - Twisting Nether"] = "Default",
+		["Jarwl - Tarren Mill"] = "Default",
+		["Jardk - Tarren Mill"] = "JarUI",
+		["Jarht - Tarren Mill"] = "JarUI",
+		["Goblinmage - Wildhammer"] = "Default",
+		["Jarmk - Twisting Nether"] = "Default",
+		["Jarev - Tarren Mill"] = "Default",
+		["Jarpp - Tarren Mill"] = "Default",
+		["Jarxd - Tarren Mill"] = "Default",
+		["Bunnyhoppy - Draenor"] = "Default",
+		["Jarwr - Tarren Mill"] = "Default",
+		["Jyr - Ravencrest"] = "Default",
+	},
+	["global"] = {
+		["optionPanelScale"] = 1.1,
+	},
+	["namespaces"] = {
+		["LibDualSpec-1.0"] = {
+		},
+	},
+	["cooldowns"] = {
+	},
+	["version"] = 3,
+	["profiles"] = {
+		["Default"] = {
+			["Party"] = {
+				["party"] = {
+					["position"] = {
+						["attach"] = "TOPLEFT",
+						["preset"] = "TOPLEFT",
+						["anchor"] = "TOPRIGHT",
+					},
+					["extraBars"] = {
+						["raidBar0"] = {
+							["manualPos"] = {
+								["raidBar0"] = {
+									["y"] = 136.7998582634609,
+									["x"] = 420.0001188718306,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		["JarUI"] = {
+			["Party"] = {
+				["party"] = {
+					["extraBars"] = {
+						["raidBar0"] = {
+							["scale"] = 0.62,
+							["columns"] = 10,
+							["manualPos"] = {
+								["raidBar0"] = {
+									["y"] = 543.2013619114296,
+									["x"] = 306.2227011701616,
+								},
+							},
+						},
+					},
+					["icons"] = {
+						["scale"] = 1,
+						["showTooltip"] = true,
+						["desaturateActive"] = true,
+					},
+					["position"] = {
+						["preset"] = "TOPLEFT",
+						["attach"] = "TOPLEFT",
+						["anchor"] = "TOPRIGHT",
+					},
+					["general"] = {
+						["showPlayer"] = true,
+					},
+				},
+			},
+			["General"] = {
+				["textures"] = {
+					["statusBar"] = {
+						["BG"] = "You Are Beautiful!",
+						["bar"] = "You Are Beautiful!",
+					},
+				},
+				["fonts"] = {
+					["statusBar"] = {
+						["font"] = "Arial Narrow",
+						["flag"] = "OUTLINE",
+					},
+					["icon"] = {
+						["font"] = "Arial Narrow",
+					},
+					["anchor"] = {
+						["font"] = "Arial Narrow",
+						["flag"] = "OUTLINE",
+					},
+				},
+			},
+		},
+	},
+}
